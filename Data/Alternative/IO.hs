fcc80991617142d4e9b04038eaaf90f2e2401bb0
Kazu Yamamoto <kazu@iij.ad.jp>
2012-02-09 12:49:20 +0900

    avoiding duplicate instances.

--- Data/Alternative/IO.hs
+++ Data/Alternative/IO.hs
@@ -1,6 +1,18 @@
 {-# LANGUAGE ScopedTypeVariables, DeriveDataTypeable #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 
+{-|
+   'IO' as 'Alternative' instance.
+
+   If the left 'IO' monad of ('<|>') causes an error or 'goNext' is used,
+   the right 'IO' monad is executed.
+
+   Of course, side effects cannot be rolled back. This means
+   that this 'Alternative' instance breaks the 'Alternative' laws.
+   But it's common in parsers.
+
+-}
+
 module Data.Alternative.IO where
 
 import Control.Applicative
@@ -8,16 +20,28 @@ import Control.Exception
 import Data.Typeable
 import Prelude hiding (catch)
 
+----------------------------------------------------------------
+
 instance Alternative IO where
   empty = goNext
   x <|> y = x `catch` (\(_ :: SomeException) -> y)
 
-data AltIOgoNext = AltIOgoNext deriving (Show, Typeable)
+----------------------------------------------------------------
 
-instance Exception AltIOgoNext
-
+{-| Go to the next 'IO' monad by throwing 'AltIOgoNext'.
+-}
 goNext :: IO a
 goNext = throwIO AltIOgoNext
 
+{-| Run any one 'IO' monad.
+-}
 runAnyOne :: [IO a] -> IO a
 runAnyOne = foldr (<|>) goNext
+
+----------------------------------------------------------------
+
+{-| Exception to control 'Alternative' 'IO'.
+-}
+data AltIOgoNext = AltIOgoNext deriving (Show, Typeable)
+
+instance Exception AltIOgoNext
--- Data/Alternative/IO/Lifted.hs
+++ Data/Alternative/IO/Lifted.hs
@@ -1,28 +1,38 @@
 {-# LANGUAGE ScopedTypeVariables, DeriveDataTypeable #-}
-{-# LANGUAGE FlexibleInstances, UndecidableInstances, FlexibleContexts #-}
-{-# OPTIONS_GHC -fno-warn-orphans #-}
+{-# LANGUAGE FlexibleContexts #-}
+
+{-|
+   Lifted 'IO' like 'Alternative' instance. Due to technical limitation,
+   Lifted 'IO' is not an instance of 'Alternative'. ('<||>') is provided
+   instead.
+-}
 
 module Data.Alternative.IO.Lifted where
 
-import Control.Applicative
 import Control.Exception (Exception, SomeException)
 import Control.Exception.Lifted (catch, throwIO)
 import Control.Monad.Base (MonadBase)
+import Control.Monad.IO.Class
 import Control.Monad.Trans.Control (MonadBaseControl)
 import Data.Typeable
 import Prelude hiding (catch)
-import Control.Monad.IO.Class
 
-instance (MonadIO m, MonadBaseControl IO m) => Alternative m where
-  empty = goNext
-  x <|> y = x `catch` (\(_ :: SomeException) -> y)
+{-| If the left 'IO' monad of ('<||>') causes an error or 'goNext' is used,
+   the right 'IO' monad is executed.
+-}
+(<||>) :: MonadBaseControl IO m => m a -> m a -> m a
+x <||> y = x `catch` (\(_ :: SomeException) -> y)
+
+{-| Go to the next 'IO' monad by throwing 'AltIOLiftedGoNext'.
+-}
+goNext :: (MonadIO m, MonadBase IO m) => m a
+goNext = throwIO AltIOLiftedGoNext
+
+{-| Run any one lifted 'IO' monad.
+-}
+runAnyOne :: (MonadIO m, MonadBaseControl IO m) => [m a] -> m a
+runAnyOne = foldr (<||>) goNext
 
 data AltIOLiftedGoNext = AltIOLiftedGoNext deriving (Show, Typeable)
 
 instance Exception AltIOLiftedGoNext
-
-goNext :: (MonadIO m, MonadBase IO m) => m a
-goNext = throwIO AltIOLiftedGoNext
-
-runAnyOne :: (MonadIO m, MonadBaseControl IO m) => [m a] -> m a
-runAnyOne = foldr (<|>) goNext
